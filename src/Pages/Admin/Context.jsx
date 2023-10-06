02c91104cc373974329d673badd6f2abcf20b98c
jhumanez099 <jhumanez.099@gmail.com>
2023-10-06 17:10:35 -0500

    Se actualizaron los modulos Document_types y Project_User

--- src/Pages/Admin/Context.jsx
+++ src/Pages/Admin/Context.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import './Context.css'
 import Headquarters from "./Pages-Admin/Headquarters";
 import Conferences from "./Pages-Admin/Conferences";
+import Document_Types from "./Pages-Admin/Document_Types";
+import Project_User from './Pages-Admin/Project_User'
 
 export default function Context() {
   return (
     <main className="main col-sm-8 col-md-9 ms-sm-auto col-lg-10 p-0">
-      <Conferences/>
+      <Project_User/>
     </main>
   );
 }
--- src/Pages/Admin/Pages-Admin/Document_Types.jsx
+++ src/Pages/Admin/Pages-Admin/Document_Types.jsx
@@ -1,7 +1,46 @@
 import React from 'react'
+import InputField from "../../../components/InputField";
+import Title from "../../../components/Title";
+import Buttons from "../../../components/Buttons";
+import Grid from "../../../components/Grid";
 
 export default function Document_Types() {
   return (
-    <div>Document_Types</div>
-  )
+    <div className="container vh-100 d-flex justify-content-center align-items-center">
+      <div className="row">
+        <div className="mb-5 d-flex justify-content-center">
+          <Title title="Tipo de Documento" />
+        </div>
+        <div className="row">
+          <div className="col-10">
+            <InputField
+              label="Nombre"
+              type="text"
+              id="Nombre-Tipo-Documento"
+              placeholder="Nombre del tipo de documento"
+            />
+          </div>
+          <div className="col-2">
+            <Buttons title="Consultar" color="white" />
+          </div>
+          <div className="row">
+            <div className="col-12">
+              <Grid />
+            </div>
+          </div>
+          <div className="container-fluid mt-4 d-flex justify-content-center">
+            <div className="col-4 d-flex justify-content-center">
+              <Buttons title="Guardar" color="white" />
+            </div>
+            <div className="col-4 d-flex justify-content-center">
+              <Buttons title="Editar" color="white" />
+            </div>
+            <div className="col-4 d-flex justify-content-center">
+              <Buttons title="Eliminar" color="white" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
