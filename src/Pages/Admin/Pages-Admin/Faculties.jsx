af27ba6ce5b37f79d814ab87d810c9ef5b634760
YisusPrime~ <santiago.ortiz02@unicatolica.edu.co>
2023-10-07 11:53:56 -0500

    Se hizo Facultades, Proyectos y otros eventos

--- src/Pages/Admin/Pages-Admin/Faculties.jsx
+++ src/Pages/Admin/Pages-Admin/Faculties.jsx
@@ -1,7 +1,43 @@
 import React from 'react'
+import InputField from "../../../components/InputField";
+import Title from "../../../components/Title";
+import Buttons from "../../../components/Buttons";
+import Grid_Muestra from "../../../components/Grid_Muestra";
 
 export default function Faculties() {
   return (
-    <div>Faculties</div>
+    <div className="container d-flex justify-content-center align-items-center">
+    <div className="row">
+      <div className="mb-5 d-flex justify-content-center">
+        <Title title="FACULTADES" />
+      </div>
+      <div className="row">
+        <form method="POST" action="./Document_Type">
+          <div className="col-10">
+            <InputField
+              label="Nombre"
+              type="text"
+              id="Nombre-Tipo-Documento"
+              placeholder="Nombre de la facultad"
+            />
+          </div>
+          <div className="col-2">
+            <Buttons title="Consultar" color="white" />
+          </div>
+          <div className="row">
+            <div className="col-12">
+              <Grid_Muestra />
+            </div>
+          </div>
+          <div className="container-fluid mt-4 d-flex justify-content-center">
+            <div className="col-4 d-flex justify-content-center">
+              <Buttons title="Guardar" color="white" />
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+
   )
 }
