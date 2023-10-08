b75401455b32b5185af416209046307b8655a6ba
Kevin <kevinospinal12@gmail.com>
2023-10-08 15:53:05 -0500

    Se agregaron los nuevos modulos de las interfaces del context y ademas se soluciono el bucle de input que tenia sedes.

--- FRONT-END/src/Pages/Admin/Pages-Admin/Programs/Programs.jsx
+++ FRONT-END/src/Pages/Admin/Pages-Admin/Programs/Programs.jsx
@@ -1,43 +1,35 @@
 import React from "react";
-import InputField from "../../../components/InputField";
-import Title from "../../../components/Title";
-import Buttons from "../../../components/Buttons";
-import Grid_Muestra from "../../../components/Grid_Muestra";
-import DropListField from "../../../components/DropListField";
+import Title from "../../../../components/Title";
+import Buttons from "../../../../components/Buttons";
+import Grid_Muestra from "../../../../components/Grid_Muestra";
+import DropListField from "../../../../components/DropListField";
+import InputField from "../../../../components/InputField";
 
-export default function Proyects() {
+export default function Programs() {
   return (
-    <div className="container d-flex justify-content-center align-items-center">
+    <div className="container vh-100 d-flex justify-content-center align-items-center">
       <div className="row">
         <div className="mb-5 d-flex justify-content-center">
-          <Title title="PROYECTOS" />
+          <Title title="PROGRAMAS" />
         </div>
-        <form method="POST" action="/Proyects">
+        <form action="/Programs" method="POST">
           <div className="row">
             <div className="col-10">
               <InputField
                 label="Nombre"
                 type="text"
-                id="Nombre-Proyecto"
-                placeholder="Nombre del proyecto"
+                id="Nombre-Programa"
+                placeholder="Nombre del programa"
               />
             </div>
             <div className="col-2">
               <Buttons title="Consultar" color="white" />
             </div>
             <div className="col-10">
-              <InputField
-                label="Descripcion"
-                type="text"
-                id="Descripcion-Proyecto"
-                placeholder="Descripcion del proyecto"
-              />
-            </div>
-            <div className="col-10">
               <DropListField
-                label="Tipo de proyecto"
+                label="Facultad"
                 id=""
-                selectOption="Seleccione el tipo de proyecto"
+                selectOption="Seleciona facultad"
               />
             </div>
             <div className="row">
