332e740f4d266f5ecf89d05b77eb995a8de7e922
Kevin <kevinospinal12@gmail.com>
2023-10-08 03:06:30 -0500

    SE HIZO EL CRUD COMPLETO DE SEDES

--- BACK-END/db.js
+++ BACK-END/db.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql')
 
+//FUNCION PARA HACER LA CONEXION A LA BASE DE DATOS
 const conexion = mysql.createConnection({
     host:'localhost',
     user:'root',
--- BACK-END/index.js
+++ BACK-END/index.js
@@ -1,65 +1,21 @@
 const express = require('express')
 const app = express();
 const cors = require('cors')
-const conexion = require('./db.js')
-const createHeadquarters = require('./Controllers/Headquarters/Headquartes.controllers.js')
+const { createHeadquarters, getHeadquarters,DeleteHeadquarters,updateHeadquarters } = require('./Controllers/Headquarters/Headquartes.controllers.js')
 app.use(cors())
 app.use(express.json())
 
-
+//METODO PARA CREAR UNA SEDE CARES MONDA
 app.post('/createHeadquarters', createHeadquarters)
 
-app.get('/getHeadquarters', (req, res) => {
-    conexion.query('SELECT * FROM tb_sedes',
-        (err, result) => {
-            if (err) {
-                console.log(err);
-                res.status(500).json({ message: 'Error al imprimir la sede' });
-            } else {
-                console.log('La mostro la sede con éxito');
-                res.json(result); // Enviar el resultado como un objeto JSON
-            }
-        }
-    )
-})
+//METODO PARA MOSTRAR UNA SEDE CARES ALEXIS NANDO CAMILO
+app.get('/getHeadquarters', getHeadquarters)
 
-app.delete('/deleteHeadquarters/:id', (req, res) => {
-    const idSede = req.params.id;
-    conexion.query(
-        'DELETE FROM tb_sedes WHERE id_sede = ?',
-        [idSede],
-        (err, result) => {
-            if (err) {
-                console.error(err);
-                res.status(500).json({ message: 'Error al eliminar la sede' });
-            } else {
-                console.log('Sede eliminada con éxito');
-                res.json({ message: 'Sede eliminada con éxito' });
-            }
-        }
-    );
-});
+//METODO PARA ELIMINAR Y VALER MONDA
+app.delete('/deleteHeadquarters/:id', DeleteHeadquarters);
 
-app.put('/updateHeadquarters/:id', (req, res) => {
-    const idSede = req.params.id;
-    const { nombre, direccion, telefono } = req.body;
-    console.log(nombre,direccion,telefono)
-
-    // Realiza la actualización en la base de datos usando el ID y los nuevos datos
-    conexion.query(
-        'UPDATE tb_sedes SET nombre_sede = ?, direccion = ?, telefono = ? WHERE id_sede = ?',
-        [nombre, direccion, telefono, idSede],
-        (err, result) => {
-            if (err) {
-                console.error(err);
-                res.status(500).json({ message: 'Error al actualizar la sede' });
-            } else {
-                console.log('Sede actualizada con éxito', result);
-                res.status(200).json({ message: 'Sede actualizada con éxito' });
-            }
-        }
-    );
-});
+//METODO PARA EDITAR LA MONDA DE LAS SEDES
+app.put('/updateHeadquarters/:id', updateHeadquarters);
 
 
 app.listen(3000, () => {
