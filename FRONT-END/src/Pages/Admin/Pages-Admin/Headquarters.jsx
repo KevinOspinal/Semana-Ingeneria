51d97c290a3e3414e75e9ccb23272579ccacc6dc
Kevin <kevinospinal12@gmail.com>
2023-10-07 23:08:02 -0500

    Se agregaron las funcionalidades de guardes y consultar en las sede, queda pendiente el eliminar editar y ademas hacer un scrol para la grid

--- FRONT-END/src/Pages/Admin/Pages-Admin/Headquarters.jsx
+++ FRONT-END/src/Pages/Admin/Pages-Admin/Headquarters.jsx
@@ -1,44 +1,73 @@
 /**SEDES */
-import React from 'react'
+import React, { useState } from 'react'
+import Axios from 'axios'
+
 import Title from '../../../components/Title'
 import InputField from '../../../components/InputField'
 import Buttons from '../../../components/Buttons'
 import Grid_Muestra from "../../../components/Grid_Muestra";
 
+
+
 export default function Headquarters() {
+
+
+  const [nombre, setnombre] = useState('')
+  const [direccion, setdireccion] = useState('')
+  const [telefono, settelefono] = useState('')
+
+  const [HeadquartersList, setHeadquartersList] = useState([])
+
+  const createHeadquarters = () =>{
+    
+    Axios.post('http://localhost:3000/createHeadquarters',{
+    nombre:nombre,
+    direccion:direccion,
+    telefono:telefono
+  }).then(()=>{
+    alert('Sede registrada.')
+  })
+  
+  }
+
+  const getHeadquarters = () =>{
+    Axios.get('http://localhost:3000/getHeadquarters').then((respond)=>{
+    setHeadquartersList(respond.data)
+  })
+  
+  }
+  console.log(HeadquartersList)
+
   return (
     <div className='container vh-100 d-flex justify-content-center align-items-center'>
       <div className='row'>
-          <div className='mb-5 d-flex justify-content-center'>
-            <Title title='SEDES' />
-          </div>
-        <form method="POST" action="./Headquarters">
-
+        <div className='mb-5 d-flex justify-content-center'>
+          <Title title='SEDES' />
+        </div>
           <div className='row'>
             <div className='col-10'>
-              <InputField label='Nombre' type='text' id='Nombre-sede' placeholder='Nombre de la sede' />
+              <InputField label='Nombre' type='text' id='Nombre-sede' placeholder='Nombre de la sede' value={(e) => { setnombre(e.target.value) }} />
             </div>
             <div className='col-2'>
-              <Buttons title='Consultar' color='white' />
+              <Buttons title='Consultar' color='white' onClick={getHeadquarters} />
             </div>
             <div className='col-10'>
-              <InputField label='Direccion' type='text' id='Direccion-sede' placeholder='Direccion de la sede' />
+              <InputField label='Direccion' type='text' id='Direccion-sede' placeholder='Direccion de la sede' value={(e) => { setdireccion(e.target.value) }} />
             </div>
             <div className='col-10'>
-              <InputField label='Telefono' type='text' id='Telefono-sede' placeholder='Telefono de la sede' />
+              <InputField label='Telefono' type='text' id='Telefono-sede' placeholder='Telefono de la sede' value={(e) => { settelefono(e.target.value) }} />
             </div>
           </div>
           <div className='row'>
             <div className='col-12'>
-              <Grid_Muestra />
+              <Grid_Muestra List={HeadquartersList}/>
             </div>
           </div>
           <div className='container-fluid mt-4 d-flex justify-content-center'>
             <div className='col-4 d-flex justify-content-center'>
-              <Buttons title='Guardar' color='white' />
+              <Buttons title='Guardar' color='white' onClick={createHeadquarters}/>
             </div>
           </div>
-        </form>
       </div>
     </div>
   );
--- FRONT-END/src/components/Buttons.jsx
+++ FRONT-END/src/components/Buttons.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-export default function Buttons({color, fontSize, title}) {
+export default function Buttons({color, fontSize, title, onClick}) {
   return (
-    <button className="btn btn-primary button text-black border-black" style={{ background: color, fontSize: fontSize}}>
+    <button className="btn btn-primary button text-black border-black" style={{ background: color, fontSize: fontSize}} onClick={onClick}>
         {title}
     </button>
   )
