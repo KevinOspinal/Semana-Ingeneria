58b2afffde4b59be2b9b76009f18a26134f40863
Kevin <kevinospinal12@gmail.com>
2023-10-13 19:13:14 -0500

    Se agrego un nuevo fondo para darle mejor estilo a la pagina

--- FRONT-END/src/Pages/Admin/Pages-Admin/Project_Type/Grid_Project_Type.jsx
+++ FRONT-END/src/Pages/Admin/Pages-Admin/Project_Type/Grid_Project_Type.jsx
@@ -1,44 +1,44 @@
 import React from "react";
 
 export default function Grid_Project_Type({ List, handleDelete, handleEdit }) {
-return (
+  return (
     <div className="container mt-5">
-    <div
+      <div
         className="scrollable-table"
         style={{ maxHeight: "18rem", overflowY: "auto" }}
-    >
+      >
         <table className="table">
-        <thead className="thead-dark">
+          <thead className="thead-dark ">
             <tr>
-            <th>Tipo Proyecto</th>
-            <th>Acciones</th>
+              <th>Tipo Proyecto</th>
+              <th>Acciones</th>
             </tr>
-        </thead>
-        <tbody>
+          </thead>
+          <tbody>
             {List
-            ? List.map((tipo_proyecto) => (
-                <tr key={tipo_proyecto.id_tipo_proyecto}>
-                <td>{tipo_proyecto.descripcion}</td>
-                <td>
-                    <button
-                    className="btn btn-danger btn-sm mr-2"
-                    onClick={() => handleDelete(tipo_proyecto.id_tipo_proyecto)}
-                    >
-                    Eliminar
-                    </button>
-                    <button
-                    className="btn btn-primary btn-sm"
-                    onClick={() => handleEdit(tipo_proyecto)}
-                    >
-                    Editar
-                    </button>
-                </td>
-                </tr>
-            ))
-            : null}
-        </tbody>
+              ? List.map((tipo_proyecto) => (
+                  <tr key={tipo_proyecto.id_tipo_proyecto}>
+                    <td>{tipo_proyecto.descripcion}</td>
+                    <td>
+                      <button
+                        className="btn btn-danger btn-sm mr-2"
+                        onClick={() => handleDelete(tipo_proyecto.id_tipo_proyecto)}
+                      >
+                        Eliminar
+                      </button>
+                      <button
+                        className="btn btn-primary btn-sm"
+                        onClick={() => handleEdit(tipo_proyecto)}
+                      >
+                        Editar
+                      </button>
+                    </td>
+                  </tr>
+                ))
+              : null}
+          </tbody>
         </table>
+      </div>
     </div>
-    </div>
-);
+  );
 }
