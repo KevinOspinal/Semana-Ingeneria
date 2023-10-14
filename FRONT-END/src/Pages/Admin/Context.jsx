5138e016bf9c080a48a397161381591d4bf204bd
Kevin <kevinospinal12@gmail.com>
2023-10-14 11:40:33 -0500

    Se cambio el fondo para que ocupe el 100 del alto

--- FRONT-END/src/Pages/Admin/Context.jsx
+++ FRONT-END/src/Pages/Admin/Context.jsx
@@ -5,7 +5,7 @@ import './Context.css'
 export default function Context({componentPage,stadePage}) {
   
   return (
-    <main className="main col-sm-8 col-md-9 ms-sm-auto col-lg-10 p-0 context-container">
+    <main className="main col-sm-8 col-md-9 ms-sm-auto col-lg-10 p-0 context-container vh-100">
       { componentPage[stadePage] }
     </main>
   );
