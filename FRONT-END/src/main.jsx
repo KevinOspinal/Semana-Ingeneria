003b35d3ce29978ec80ac022c23500a696c59046
Kevin <kevinospinal12@gmail.com>
2023-10-15 02:28:34 -0500

    Se avanzo con la barra de navegacion y el hero principal, aun falta ajustarlo y por parte de las tarjetas quedaron bien

--- FRONT-END/src/main.jsx
+++ FRONT-END/src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'; // Importa el archivo CSS de Bootstrap
 import 'bootstrap/dist/js/bootstrap.min.js'; // Importa el archivo JavaScript de Bootstrap (opcional, solo si necesitas funcionalidad JavaScript)
+import 'bootstrap-icons/font/bootstrap-icons.css'; // Importa la hoja de estilos de Bootstrap Icons
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
