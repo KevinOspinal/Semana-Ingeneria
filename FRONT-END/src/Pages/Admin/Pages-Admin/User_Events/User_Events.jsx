1b89fb02b93447ecc63ede249af2d47ada81bcfe
jhumanez099 <jhumanez.099@gmail.com>
2023-10-23 21:42:53 -0500

    Ajustes

--- FRONT-END/src/Pages/Admin/Pages-Admin/User_Events/User_Events.jsx
+++ FRONT-END/src/Pages/Admin/Pages-Admin/User_Events/User_Events.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Title from "../../../../components/Title";
 import Buttons from "../../../../components/Buttons";
 import DropListField from "../../../../components/DropListField";
+import InputField from "../../../../components/InputField";
 
 export default function User_Events() {
   return (
@@ -10,32 +11,36 @@ export default function User_Events() {
         <div className="mb-5 d-flex justify-content-center">
           <Title title="USUARIO POR EVENTO" />
         </div>
+        <div className="row">
+          <div className="col-10">
+            <InputField
+              label="Cedula"
+              type="text"
+              id="Cedula-Usuario-Conferencia"
+              placeholder="Documento"
+            />
+          </div>
+          <div className="col-10">
+            <DropListField
+              label="Evento"
+              id=""
+              selectOption="Seleciona evento"
+            />
+          </div>
           <div className="row">
-            <div className="col-10">
-              <DropListField
-                label="Usuario"
-                id=""
-                selectOption="Seleciona Usuario"
+            <div className="col-12">//grid</div>
+          </div>
+          <div className="container-fluid mt-4 d-flex justify-content-center">
+            <div className="col-4 d-flex justify-content-center">
+              <Buttons
+                title="Guardar"
+                color="white"
+                colorbutton="black"
+                type="submit"
               />
-            </div>
-            <div className="col-10">
-              <DropListField
-                label="Evento"
-                id=""
-                selectOption="Seleciona evento"
-              />
-            </div>
-            <div className="row">
-              <div className="col-12">
-                //grid
-              </div>
-            </div>
-            <div className="container-fluid mt-4 d-flex justify-content-center">
-              <div className="col-4 d-flex justify-content-center">
-                <Buttons title="Guardar" color="white" colorbutton='black' type="submit" />
-              </div>
             </div>
           </div>
+        </div>
       </div>
     </div>
   );
