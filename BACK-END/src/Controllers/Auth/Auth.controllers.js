9164757dbfac47579c7bacbf151ddcbd39af06c9
jhumanez099 <jhumanez.099@gmail.com>
2023-10-25 22:00:20 -0500

    Actualizacion

--- BACK-END/src/Controllers/Auth/Auth.controllers.js
+++ BACK-END/src/Controllers/Auth/Auth.controllers.js
@@ -1,4 +1,4 @@
-// Importación de módulos y funciones necesarios
+//Importación de módulos y funciones necesarios
 const conexion = require('../../../db'); // Conexión a la base de datos
 const bcrypt = require('bcryptjs'); // Módulo para el hash de contraseñas
 const CreateAccessToken = require('../../libs/jwt.js'); // Función para crear tokens JWT
@@ -31,11 +31,11 @@ const Register = async (req, res) => {
         const token = await CreateAccessToken({ id: result.insertId });
 
         // Establecer una cookie en la respuesta (esto podría usarse para autenticación posterior)
-        res.cookie('token', {token});
+        res.cookie('token', { token });
 
         // Responder con un código 200 y un mensaje
         res.status(200).json({ message: 'Usuario agregado con éxito', token });
-        
+
     } catch (error) {
         // Manejo de errores en caso de falla
         console.log(error);
@@ -44,9 +44,62 @@ const Register = async (req, res) => {
 }
 
 // Función para el inicio de sesión (aún no está implementada)
-const Login = (req, res) => {
-    res.send('LOGIADO');
+const Login = async (req, res) => {
+    // Extracción de datos del cuerpo de la solicitud HTTP
+    const { correo, documento } = req.body;
+
+    try {
+        const result = await new Promise((resolve, reject) => {
+            conexion.query(
+                'SELECT * FROM tb_usuarios WHERE correo = ?',
+                [correo],
+                (error, results) => {
+                    if (error) {
+                        reject(error);
+                    } else {
+                        resolve(results);
+                    }
+                }
+            )
+        })
+        if (result.length > 0) {
+            const usuario = result[0].correo;
+            console.log(usuario);
+
+            // Aquí puedes continuar con el proceso de autenticación
+        } else {
+            // No se encontró ningún usuario con el correo proporcionado
+            return res.status(400).json({ message: "Usuario no encontrado" });
+        }
+
+        const isMach = await bcrypt.compare(documento, result[0].contraseña)
+        if (!isMach) return res.status(400).json({ message: "Contraseña incorrecta" })
+        // Creación de un token de acceso JWT para el usuario recién registrado
+        const token = await CreateAccessToken({ id: result[0].id_usuario });
+
+        // Establecer una cookie en la respuesta (esto podría usarse para autenticación posterior)
+        res.cookie('token', { token });
+
+        // Responder con un código 200 y un mensaje
+        res.status(200).json({ message: 'Usuario iniciado con exito', token });
+
+    } catch (error) {
+        // Manejo de errores en caso de falla
+        console.log(error);
+        res.status(500).json({ message: 'Error al iniciar sesion' });
+    }
+}
+
+const Logout = async (req, res) =>{
+    res.cookie('token', "", {
+        expires: new Date(0)
+    })
+    return res.sendStatus(200)
+}
+
+const Profile = async (req, res) => {
+    
 }
 
 // Exportar las funciones para su uso en otros lugares del código
-module.exports = { Register, Login };
+module.exports = { Register, Login, Logout, Profile };
--- BACK-END/src/router/auth.routes.js
+++ BACK-END/src/router/auth.routes.js
@@ -1,9 +1,11 @@
 const {Router} = require('express')
 const router = Router();
-const {Register,Login} = require('../Controllers/Auth/Auth.controllers.js')
+const {Register,Login,Logout} = require('../Controllers/Auth/Auth.controllers.js')
 
 
 router.post('/register',Register)
 router.post('/login',Login)
+router.post('/logout',Logout)
+router.get('/profile',)
 
 module.exports = router;
